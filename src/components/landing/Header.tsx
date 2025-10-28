c7594810a20ef5a46a1180f472ac854c16afa3ea
Fernando Trejo <fernandotrejo159@gmail.com>
2025-10-28 16:09:54 +0000

    cambia este para que pueda colocar un logo (_for element svg_)

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -1,15 +1,11 @@
-import { MountainIcon } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="px-4 lg:px-6 h-20 flex items-center bg-background shadow-sm sticky top-0 z-50">
       <Link href="/" className="flex items-center justify-center" prefetch={false}>
-        <MountainIcon className="h-8 w-8 text-primary" />
-        <div className="ml-3">
-            <div className="text-sm font-semibold tracking-widest text-primary">RULES OF ORIGIN</div>
-            <div className="text-sm font-semibold tracking-widest text-primary">TRADE SOLUTIONS</div>
-        </div>
+        <Image src="https://picsum.photos/seed/logo/150/40" alt="Roots.trade Logo" width={150} height={40} className="h-10 w-auto" />
       </Link>
     </header>
   );
